552ebe0ac89f0d6322e35e4d604fcee2741f9016
syuan0212@gmail.com <hsuan777>
2020-07-22 00:03:56 +0800

    feat:加入購物車

--- app/assets/js/index.js
+++ app/assets/js/index.js
@@ -3,7 +3,8 @@
   data:{
     hexAPI: {
       personID: "85a8cd22-1b7d-43af-9b5a-5aa679129559",
-      data: []
+      data: [],
+      dataShopping:[]
     },
     selectPay:[`ATM`, `信用卡`],
     person:{
@@ -11,6 +12,10 @@
       email:"",
       phone:"",
       address:"",
+    },
+    temporary: {
+      product: "",
+      quantity: "1",
     },
   },
   methods:{
@@ -21,11 +26,29 @@
           vm.hexAPI.data = response.data.data;
         })
     },
-    order(){
-      
-    }
+    addShopping(id){
+      let vm = this;
+      vm.temporary.product = id;
+      axios.post(`https://course-ec-api.hexschool.io/api/${vm.hexAPI.personID}/ec/shopping`, vm.temporary)
+        .then(() => {
+          console.log(`加入成功`);
+          vm.getShopping();
+        })
+    },
+    getShopping(){
+      let vm = this;
+      axios.get(`https://course-ec-api.hexschool.io/api/${vm.hexAPI.personID}/ec/shopping`)
+        .then((response) => {
+          vm.hexAPI.dataShopping = response.data.data;
+          console.log(vm.hexAPI.dataShopping);
+        })
+    },
   },
   created(){
     this.getData();
+    this.getShopping();
+    
+  },
+  mounted(){
   }
 })
